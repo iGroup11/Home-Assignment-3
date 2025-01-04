772aa2974af473f314676d0b6699d805473dfad0
batelta <bateltasama3@gmail.com>
2025-01-04 16:47:02 +0200

    HW3

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,84 @@
+﻿using HW3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HW3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            User user = new User();
+            return user.read();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public int Post([FromBody] User user)
+        {
+            return user.insert();
+        }
+        [HttpPost("SearchUser")]
+        public User searchUser([FromBody] User user)
+        {
+            return user.searchUser(); // This will return the UsersList
+        }
+
+
+        /*
+        public User searchUser(string email,string password)
+        {
+            User user = new User();
+
+            return user.searchUser(email,password); // This will return the UsersList
+        }
+        */
+        [HttpPut("{id}")]
+        public int Put(int id, [FromBody] User user)
+        {
+            return user.updateUserDetails(id);
+
+        }
+        /* [HttpPost("SearchUser")] ///QueryString for searching user
+         public User searchUser(string email)
+         {
+             User user=new User();
+             return user.searchUser(email);
+         }
+        */
+        // PUT api/<UsersController>/5
+        //          [HttpPut("{id}")]
+        //    public void Put(int id, [FromBody] string value)
+        //   {
+        //  }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        ///new method to get logged user 
+        ///
+        [HttpGet("AddGametoUser")]
+        public int UserGame(int userId, int gameId)
+        {
+
+            User user = new User();
+            return user.AddGametoUser(userId, gameId);
+
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics.Eventing.Reader;
+
+namespace HW3.Models
+{
+    public class User
+    {
+        int id;
+        string name;
+        string email;
+        string password;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(int id,string name,string email,string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public User() //empty ctor
+        {
+            
+        }
+        public List<User> UsersList = new List<User>(); //creating user list
+
+        DBservices dbs = new DBservices();
+
+        public int insert()
+        {
+            return dbs.InsertUser(this);
+        }
+
+        public List<User> read() 
+        {
+            return dbs.ReadUsers();
+        }
+        /*  public User searchUser(string email)
+          {
+
+          DBservices dbs = new DBservices();
+             return dbs.findUser(email);
+          }
+        */
+
+
+        /*
+      public int updateUserDetails(int id, string email, string name, string password)
+        {
+            return dbs.updateUserDetails(id, email, name, password);
+        }
+        public User searchUser(string email,string password)
+        {
+
+            return dbs.findUser(email, password);
+        }
+        */
+        public int updateUserDetails(int id)
+        {
+            return dbs.updateUserDetails(id, this.Email, this.Name, this.Password);
+        }
+        public User searchUser()
+        {
+
+            return dbs.findUser(this.Email, this.Password);
+        }
+
+        public int AddGametoUser(int uId, int gId)
+        {
+            return dbs.AddGametoUser(uId, gId);
+
+        }
+    }
+}
